05eb213e79c3072daf905be7a918dc80cddd2473
Bhaumik Kore <bhaumik.kore31@gmail.com>
2023-06-08 13:34:30 +0530

    Added fonts "Poppins" & "Comfortaa"

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,13 @@
 </script>
 
 <slot />
+
+<style lang="postcss">
+	/* @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap'); */
+	:global(:root) {
+		background-color: #edebe3;
+		font-family: 'Comfortaa', sans-serif;
+		font-weight: theme('fontWeight.medium');
+		letter-spacing: 0.2px;
+	}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        comfortaa: ['Comfortaa', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif']
+      }
+    }
   },
   plugins: []
 };
