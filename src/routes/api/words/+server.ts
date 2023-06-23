c73280e08334e57c39a9f778c09bbfbb6721735b
Bhaumik Kore <bhaumik.kore31@gmail.com>
2023-06-23 16:10:41 +0530

    temp

--- src/routes/api/words/+server.ts
+++ src/routes/api/words/+server.ts
@@ -9,11 +9,11 @@ import pureHindi from './languages/hindi.json';
 export const GET: RequestHandler = ({ url }) => {
   const limit = Number(url.searchParams.get('limit'));
   const lang = Number(url.searchParams.get('lang'));
-  // const words = english.words.slice(0, limit).sort(() => 0.5 - Math.random());
+  const words = english.words.slice(0, limit).sort(() => 0.5 - Math.random());
 
   // const words = python.words.slice(0, limit).sort(() => 0.5 - Math.random());
   // const words = js.words.slice(0, limit).sort(() => 0.5 - Math.random())
-    const words = pureHindi.words.slice(0, limit).sort(() => 0.5 - Math.random())
+    // const words = pureHindi.words.slice(0, limit).sort(() => 0.5 - Math.random())
     // const words = hindi.words.slice(0, limit).sort(() => 0.5 - Math.random())
   return json(words);
 };
