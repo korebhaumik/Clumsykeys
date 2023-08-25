d3000a95dcbaafe36bda269cacccd91f283095af
Bhaumik Kore <bhaumik.kore31@gmail.com>
2023-08-25 17:05:26 +0530

    set config to edge

--- src/routes/callback/+server.ts
+++ src/routes/callback/+server.ts
@@ -1,5 +1,9 @@
 import { redirect } from '@sveltejs/kit'
 
+export const config = {
+	runtime: 'edge'
+};
+
 export const GET = async ({ url, locals: { supabase } }) => {
   const code = url.searchParams.get('code')
 
